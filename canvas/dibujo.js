3743197fad216f123b2df698b193065a75627e79
tatyana2810 <99229571+tatyana2810@users.noreply.github.com>
2022-02-15 10:21:43 -0500

    Update dibujo.js

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,8 +1,20 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#0E6251"
 
-dibujarLinea("purple", 10, 300, 220, 10);
-dibujarLinea("black", 35, 234, 175, 278);
+for(l=0; l < lineas; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("linea " + 1);
+}
+
+dibujarLinea(colorcito, 1, 1, 1, 299);
+dibujarLinea(colorcito, 1, 299, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
